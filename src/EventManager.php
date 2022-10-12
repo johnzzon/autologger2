d2546b838d423ee9ccdd1f7e0489fac12b83ee5d
Emil Johnsson <emil@kodamera.se>
2022-10-12 06:49:50 +0200

    Added methods to get weekly summary

--- src/EventManager.php
+++ src/EventManager.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Cassandra\Date;
 use DateTime;
 use Sabre\VObject\Component\VEvent;
 use Sabre\VObject\Reader;
@@ -30,11 +31,11 @@ class EventManager
     public function __construct(string $calendar_link, DateTime $date_start, DateTime $date_end)
     {
         $vcalendar = Reader::read(
-            fopen($calendar_link,'rb')
+            fopen($calendar_link, 'rb')
         );
 
         if (empty($vcalendar->VEVENT)) {
-          return;
+            return;
         }
 
         foreach ($vcalendar->VEVENT as $vevent) {
@@ -111,7 +112,54 @@ class EventManager
         $interval = $zero->diff($total_duration);
         $hours = $interval->h;
         $minutes = $interval->i;
+
         return $hours + ($minutes / 60);
+    }
+
+    /**
+     * Get total duration of the day.
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function getWeeklyTotalDuration(): array
+    {
+        $weeks = $this->getWeeklyEvents();
+        $weeks = array_reverse($weeks);
+        $week_durations = [];
+
+        foreach ($weeks as $week => $events) {
+            $zero = new DateTime('00:00');
+            $total_duration = clone $zero;
+
+            foreach ($events as $event) {
+                $total_duration->add($event->getDurationInterval());
+            }
+            $interval = $zero->diff($total_duration);
+            $days = $interval->d;
+            $hours = $interval->h;
+            $minutes = $interval->i;
+            $week_durations[$week] = number_format(($days * 24) + $hours + ($minutes / 60), 2);
+        }
+
+        return $week_durations;
+    }
+
+    public function getWeeklyEvents(): array
+    {
+        $weeks = [];
+
+        foreach ($this->getEvents() as $event) {
+            $year_and_week = $event->getDate()->format('Y, \vW');
+            $year = $event->getDate()->format('Y');
+            $week = $event->getDate()->format('W');
+            $week_date = new DateTime();
+            $week_date->setISODate($year, $week);
+            $weeks[$week_date->format('Y-m-d')][] = $event;
+        }
+
+        return array_reverse($weeks);
     }
 
 }
